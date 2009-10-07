b3aba44df5b3dae0c86d3dc3c13a41655afb3bc9
glacasse <glacasse@e58de720-aa09-11de-8ba5-91705c1537ed>
2009-10-07 01:24:00 +0000

    Created argument class for proof.AddArgument optimization

---
+++ trunk/Riasce/Ai/Brain/Memory/Concept/ConnectionBranch/Argument.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AntiCulture.Kid
+{
+    /// <summary>
+    /// This class represents a proof's argument
+    /// </summary>
+    class Argument
+    {
+        #region Fields
+        private Concept subject, verb, complement;
+        #endregion
+
+        #region Constructor
+        public Argument(Concept subject, Concept verb, Concept complement)
+        {
+            this.subject = subject;
+            this.verb = verb;
+            this.complement = complement;
+        }
+        #endregion
+
+        #region Properties
+        public Concept Subject
+        {
+            get { return subject; }
+        }
+
+        public Concept Verb
+        {
+            get { return verb; }
+        }
+
+        public Concept Complement
+        {
+            get { return complement; }
+        }
+        #endregion
+    }
+}
